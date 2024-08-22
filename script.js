982bde082e6b7cb228385344ab734d6976a31204
joao3313 <ikkijp2012@gmail.com>
2024-08-22 19:36:02 -0300

    feat: Programa

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("Alerta de Confirmação!");
+//alert("Alerta de Confirmação!");
 
 var nome1 = "";
 let nota1, nota2, nota3 = 0;
@@ -16,14 +16,17 @@ var media = (nota1 + nota2)/Provas ;
     if(media >= 6)
         if(media==10)
             
-         alert(nome1 +", "+ "Uau! Aprovado")
-        else
+         alert(nome1 +", "+ "Uau! Aprovado"+media)
+        else if (media <=4) {
 
-        alert(nome1 +", "+ "Aluno em recuperação! Media "+media)
+          alert(nome1 +", "+ "Aluno em recuperação! Media "+media)
+        }
+
+        
                 
        else
        
-       alert(nome1 +", "+ "Reprovado!")
+       alert(nome1 +", "+ "Reprovado!"+media)
        
         
 }
