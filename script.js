ce3f2b6c558ab6527027af47410bb9f2acdf485e
joao3313 <ikkijp2012@gmail.com>
2024-08-22 19:18:33 -0300

    feat: Codigo base

---
+++ script.js
@@ -0,0 +1,29 @@
+alert("Alerta de Confirmação!");
+
+var nome1 = "";
+let nota1, nota2, nota3 = 0;
+const Provas = 2;
+
+nome1 = prompt("Digite o Nome do Aluno:");
+nota1 = parseFloat(prompt("Digite a Nota 1: "));
+nota2 = parseFloat(prompt("Digite a Nota 2: "));
+
+
+var media = (nota1 + nota2)/Provas ;
+
+{
+
+    if(media >= 6)
+        if(media==10)
+            
+         alert(nome1 +", "+ "Uau! Aprovado");
+        else
+        
+         alert(nome1 +", "+ "Aluno em recuperação! Media "+media);
+       else
+       
+        alert(nome1 +", "+ "Reprovado!")
+}
+
+
+alert(nome1 + mensagem);
