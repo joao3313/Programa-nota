a641925ef07b4f2d9c1ecdd3b43b416873ce6705
joao3313 <ikkijp2012@gmail.com>
2024-08-23 19:02:25 -0300

    feat:Exercicio04

--- exercicio04.js
+++ exercicio04.js
@@ -3,19 +3,22 @@ let nome = prompt("Informe seu nome:");
 let Numero_Escolhido01 = parseInt(prompt("Informe um número:"));
 let Numero_Escolhido02 = parseInt(prompt("Informe um número:"));
 let Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
-let mensagem = "";
+//let mensagem = "";
 
 {
 
-    if (Numero_Escolhido01 > Numero_Escolhido02) {
+    if (Numero_Escolhido01 > Numero_Escolhido02 || Numero_Escolhido01 > Numero_Escolhido03) {
 
-        alert(nome +" ,"+  "você escolheu o número:"+" "+ Numero_Escolhido01);
+        alert(nome + " ," + "você escolheu o número:" + " " + Numero_Escolhido01);
     }
 
-     else (Numero_Escolhido01 < Numero_Escolhido02)
-     {
+    else if (Numero_Escolhido02 > Numero_Escolhido01 || Numero_Escolhido02 > Numero_Escolhido03) {
 
-       alert(nome + ", " + "você escolheu o número:"+" "+ Numero_Escolhido02);
-   }
-     
-  }
+        alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido02);
+    }
+    else  (Numero_Escolhido03 > Numero_Escolhido01 || Numero_Escolhido03 > Numero_Escolhido02); {
+
+        alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido03);
+    }
+
+}
