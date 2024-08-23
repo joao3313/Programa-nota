f3557fc3019a84e0473e8f410ed1febbb027c7d3
joao3313 <ikkijp2012@gmail.com>
2024-08-22 21:15:01 -0300

    feat:Exercicio02

--- exercicio02.js
+++ exercicio02.js
@@ -4,36 +4,42 @@ let dia_escolhido = prompt("Informe o dia da semana a exibir:");
 let mensagem = "";
 
 if (dia_escolhido == 0);
-    else if (dia_escolhido == 1){
-    
-        alert (nome+ ", "+"Hoje é Domingo"); }
-    
+else if (dia_escolhido == 1) {
+
+    alert(nome + ", " + "Hoje é Domingo");
+}
+
 else if (dia_escolhido == 2) {
-    
-    alert (nome+ ", "+"Hoje é Segunda Feira"); }
+
+    alert(nome + ", " + "Hoje é Segunda Feira");
+}
 
 else if (dia_escolhido == 3) {
-    
-    alert (nome+ ", "+"Hoje é Terça Feira"); }
 
+    alert(nome + ", " + "Hoje é Terça Feira");
+}
 
 else if (dia_escolhido == 4) {
-    
-    alert (nome+ ", "+"Hoje é Quarta Feira"); }
+
+    alert(nome + ", " + "Hoje é Quarta Feira");
+}
 
 
-else if (dia_escolhido == 5){
-    
-        alert (nome+ ", "+"Hoje é Quinta Feira"); }
+else if (dia_escolhido == 5) {
+
+    alert(nome + ", " + "Hoje é Quinta Feira");
+}
 
 
-else if (dia_escolhido == 6){
-    
-        alert (nome+ ", "+"Hoje é Sexta Feira"); }
+else if (dia_escolhido == 6) {
 
-else if (dia_escolhido == 7){
-    
-    alert (nome+ ", "+"Hoje é Sábado"); }
+    alert(nome + ", " + "Hoje é Sexta Feira");
+}
+
+else if (dia_escolhido == 7) {
+
+    alert(nome + ", " + "Hoje é Sábado");
+}
 
 
-alert (mensagem);
+//alert(mensagem);
