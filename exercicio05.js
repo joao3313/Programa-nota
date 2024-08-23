9e3be48717706b6201a474ff7b3e9ae5ebb56778
joao3313 <ikkijp2012@gmail.com>
2024-08-23 20:24:47 -0300

    feat:Exercicio05

--- exercicio05.js
+++ exercicio05.js
@@ -1,12 +1,12 @@
 let nome = prompt("Informe seu nome:");
 
-let Numero_Escolhido01 = parseInt(prompt("Informe um número:"));
-let Numero_Escolhido02 = parseInt(prompt("Informe um número:"));
-let Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
+var Numero_Escolhido01 = parseInt(prompt("Informe um número:"));
+var Numero_Escolhido02 = parseInt(prompt("Informe um número:"));
+var Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
 //let mensagem = "";
-
-var maior = (Numero_Escolhido01,Numero_Escolhido02,Numero_Escolhido03);
-var menor = (Numero_Escolhido01,Numero_Escolhido02,Numero_Escolhido03);
+var maior =0;
+var menor;
+var numero;
 {
 
     if ((Numero_Escolhido01 > Numero_Escolhido02) && (Numero_Escolhido01 > Numero_Escolhido03)) {
@@ -16,7 +16,7 @@ var menor = (Numero_Escolhido01,Numero_Escolhido02,Numero_Escolhido03);
 
     else if ((Numero_Escolhido01 <= Numero_Escolhido02) && (Numero_Escolhido01 <= Numero_Escolhido03)) {
 
-        alert(nome + " ," + "você escolheu o número:" + " " (maior)&&(menor) );
+        alert(nome + " ," + "você escolheu o número:" + " " );
     }
 
     else if ((Numero_Escolhido02 > Numero_Escolhido01) && (Numero_Escolhido02 > Numero_Escolhido03))
