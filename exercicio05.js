18b69aa6cbc58329f5cf246dc31eea40560d3487
joao3313 <ikkijp2012@gmail.com>
2024-08-23 20:00:20 -0300

    feat:Exercicio05

---
+++ exercicio05.js
@@ -0,0 +1,40 @@
+let nome = prompt("Informe seu nome:");
+
+let Numero_Escolhido01 = parseInt(prompt("Informe um número:"));
+let Numero_Escolhido02 = parseInt(prompt("Informe um número:"));
+let Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
+//let mensagem = "";
+
+
+{
+
+    if ((Numero_Escolhido01 > Numero_Escolhido02) && (Numero_Escolhido01 > Numero_Escolhido03)) 
+        
+        {
+
+        alert(nome + " ," + "você escolheu o número:" + " " + Numero_Escolhido01);
+    } 
+    
+    else if ((Numero_Escolhido01 <= Numero_Escolhido02) && (Numero_Escolhido01 <= Numero_Escolhido03)){
+
+        alert(nome + " ," + "você escolheu o número:" + " " );
+    }
+
+    else if ((Numero_Escolhido02 > Numero_Escolhido01) && (Numero_Escolhido02 > Numero_Escolhido03))
+        
+        {
+
+        alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido02);
+    }
+
+
+    else if ((Numero_Escolhido03 > Numero_Escolhido01) && (Numero_Escolhido03 > Numero_Escolhido02))
+        
+        
+        {
+
+        alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido03);
+
+    }
+
+}
