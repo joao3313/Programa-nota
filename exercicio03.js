29eee3fe1e60e1b608f0ccb302da82bb2a00e0b4
joao3313 <ikkijp2012@gmail.com>
2024-08-22 21:29:23 -0300

    feat:Exercicio03

---
+++ exercicio03.js
@@ -0,0 +1,16 @@
+let nome = prompt("Informe seu nome:");
+
+let Numero_Escolhido = prompt("Informe um número:");
+let mensagem = "";
+
+{
+
+    if (Numero_Escolhido >=0 ) {
+
+      alert( + ", " + "");
+
+    } else if (Numero_Escolhido >= 0) {
+         alert(nome1 + ", " + "Recuperação!"); }
+
+     
+  }
