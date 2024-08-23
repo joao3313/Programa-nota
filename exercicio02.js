916113e2906404e725bdbaacb2c889babd712ef0
joao3313 <ikkijp2012@gmail.com>
2024-08-22 21:08:10 -0300

    feat:Exercicio02

--- exercicio02.js
+++ exercicio02.js
@@ -3,25 +3,36 @@ let nome = prompt("Informe seu nome:");
 let dia_escolhido = prompt("Informe o dia da semana a exibir:");
 let mensagem = "";
 
-if (dia_escolhido)
+if (dia_escolhido == 0)
     else if () {
     
 }
-else if () {
+else if (dia_escolhido == 1) {
+    
+    alert (nome+ ", "+"Hoje é Segunda Feira"); }
 
-}
-else if () {
+else if (dia_escolhido == 2) {
+    
+    alert (nome+ ", "+"Hoje é Segunda Feira"); }
 
-}
-else if () {
 
-}
-else if () {
+else if (dia_escolhido == 3) {
+    
+    alert (nome+ ", "+"Hoje é Segunda Feira"); }
 
-}
-else if () {
 
-}
-else if () {
+else if (dia_escolhido == 4){
+    
+        alert (nome+ ", "+"Hoje é Segunda Feira"); }
 
-}
+
+else if (dia_escolhido == 5){
+    
+        alert (nome+ ", "+"Hoje é Segunda Feira"); }
+
+else if (dia_escolhido == 6){
+    
+    alert (nome+ ", "+"Hoje é Segunda Feira"); }
+
+
+alert (mensagem);
