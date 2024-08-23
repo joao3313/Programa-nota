b03d7698576216932f6a1f3cc009315f6e6c34c9
joao3313 <ikkijp2012@gmail.com>
2024-08-23 20:41:52 -0300

    feat:Exercicio05

--- exercicio05.js
+++ exercicio05.js
@@ -1,36 +1,32 @@
 let nome = prompt("Informe seu nome:");
 
-var Numero_Escolhido01 = parseInt(prompt("Informe um número:"));
-var Numero_Escolhido02 = parseInt(prompt("Informe um número:"));
-var Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
+let Numero_Escolhido01 = parseInt(prompt("Informe um número:"));
+let Numero_Escolhido02 = parseInt(prompt("Informe um número:"));
+let Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
 //let mensagem = "";
 var maior =0;
 var menor;
 var numero;
+
 {
 
-    if ((Numero_Escolhido01 > Numero_Escolhido02) && (Numero_Escolhido01 > Numero_Escolhido03)) {
+    if ((Numero_Escolhido01 > Numero_Escolhido02) && (Numero_Escolhido01 > Numero_Escolhido03) && (maior>=menor) && (menor>=maior)) {
 
-        alert(nome + " ," + "você escolheu o número:" + " " + Numero_Escolhido01 && maior);
+        alert(nome + " ," + "você escolheu o número:" + " " + Numero_Escolhido01);
     }
 
-    else if ((Numero_Escolhido01 <= Numero_Escolhido02) && (Numero_Escolhido01 <= Numero_Escolhido03)) {
+    else if ((Numero_Escolhido02 > Numero_Escolhido01) && (Numero_Escolhido02 > Numero_Escolhido03)) {
 
-        alert(nome + " ," + "você escolheu o número:" + " "+ numero );
+        alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido02);
     }
 
-    else if ((Numero_Escolhido02 > Numero_Escolhido01) && (Numero_Escolhido02 > Numero_Escolhido03))
 
-        
-       // else if ((Numero_Escolhido02 <= Numero_Escolhido01) && (Numero_Escolhido02 <= Numero_Escolhido03)) {
+    else if ((Numero_Escolhido03 > Numero_Escolhido01) && (Numero_Escolhido03 > Numero_Escolhido02)) {
 
-        alert(nome + " ," + "você escolheu o número:" + " ");
+        alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido03);
 
     }
-    //else if ((Numero_Escolhido03 > Numero_Escolhido01) && (Numero_Escolhido03 > Numero_Escolhido02)) {
 
-        alert(nome + ", " + "você escolheu o número:" + " ");
+}
 
-   // }
 
-//}
