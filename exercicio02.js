2ed1206991e6f7866e4e6a36f1064694c1079ff0
joao3313 <ikkijp2012@gmail.com>
2024-08-22 21:01:37 -0300

    feat:Exercicio02

---
+++ exercicio02.js
@@ -0,0 +1,27 @@
+let nome = prompt("Informe seu nome:");
+
+let dia_escolhido = prompt("Informe o dia da semana a exibir:");
+let mensagem = "";
+
+if (dia_escolhido)
+    else if () {
+    
+}
+else if () {
+
+}
+else if () {
+
+}
+else if () {
+
+}
+else if () {
+
+}
+else if () {
+
+}
+else if () {
+
+}
