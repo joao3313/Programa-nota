158f0d9e88ced1eaec229d0aa0ccc0de5cf54e1e
joao3313 <ikkijp2012@gmail.com>
2024-08-23 19:42:51 -0300

    feat:Exercicio04

--- exercicio04.js
+++ exercicio04.js
@@ -28,7 +28,7 @@ let Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
     {
 
        // alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido03);
-       alert(nome + ", " + "você escolheu o número:" + Numero_Escolhido01 && Numero_Escolhido02 && Numero_Escolhido03);
+       alert(nome + ", " + "você escolheu o número:" + Numero_Escolhido01 || Numero_Escolhido02 || Numero_Escolhido03);
     }
    
 }
