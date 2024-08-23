95387d2872610317252e08ddb4780a1a1777362a
joao3313 <ikkijp2012@gmail.com>
2024-08-22 21:57:07 -0300

    feat:Exercicio03

--- exercicio04.js
+++ exercicio04.js
@@ -2,6 +2,7 @@ let nome = prompt("Informe seu nome:");
 
 let Numero_Escolhido01 = parseInt(prompt("Informe um número:"));
 let Numero_Escolhido02 = parseInt(prompt("Informe um número:"));
+let Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
 let mensagem = "";
 
 {
@@ -11,10 +12,10 @@ let mensagem = "";
         alert(nome +" ,"+  "você escolheu o número:"+" "+ Numero_Escolhido01);
     }
 
-     //else (Numero_Escolhido01 < Numero_Escolhido02)
-     //{
+     else (Numero_Escolhido01 < Numero_Escolhido02)
+     {
 
-      // alert(nome + ", " + "você escolheu o número:"+" "+ Numero_Escolhido02);
-  // }
+       alert(nome + ", " + "você escolheu o número:"+" "+ Numero_Escolhido02);
+   }
      
   }
