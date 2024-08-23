e57ee89591bbad6c866e14c5441756376544d702
joao3313 <ikkijp2012@gmail.com>
2024-08-23 19:46:03 -0300

    feat:Exercicio04

--- exercicio04.js
+++ exercicio04.js
@@ -8,14 +8,14 @@ let Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
 
 {
 
-    if (Numero_Escolhido01 > Numero_Escolhido02 == Numero_Escolhido01 > Numero_Escolhido03)
+    if (Numero_Escolhido01 > Numero_Escolhido02 && Numero_Escolhido01 > Numero_Escolhido03)
         
         {
 
        // alert(nome + " ," + "você escolheu o número:" + " " + Numero_Escolhido01);
     }
 
-    else if (Numero_Escolhido02 > Numero_Escolhido01 == Numero_Escolhido02 > Numero_Escolhido03)
+    else if (Numero_Escolhido02 > Numero_Escolhido01 && Numero_Escolhido02 > Numero_Escolhido03)
         
         {
 
@@ -23,12 +23,12 @@ let Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
     }
 
 
-    else  (Numero_Escolhido03 > Numero_Escolhido01 == Numero_Escolhido03 > Numero_Escolhido02)
+    else  (Numero_Escolhido03 > Numero_Escolhido01 && Numero_Escolhido03 > Numero_Escolhido02)
     
     {
 
        // alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido03);
-       alert(nome + ", " + "você escolheu o número:" + Numero_Escolhido01 || Numero_Escolhido02 || Numero_Escolhido03);
+       alert(nome + ", " + "você escolheu o número:" + "");
     }
    
 }
