d1203cf23676de3b187ea0ee67acc5dc7d713ac7
joao3313 <ikkijp2012@gmail.com>
2024-08-23 21:26:08 -0300

    feat:Exercicio05

--- exercicio04.js
+++ exercicio04.js
@@ -8,18 +8,18 @@ let Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
 
 {
 
-    if ((Numero_Escolhido01 > Numero_Escolhido02) && (Numero_Escolhido01 > Numero_Escolhido03)) {
+    if ((Numero_Escolhido01 >= Numero_Escolhido02) && (Numero_Escolhido01 >= Numero_Escolhido03)) {
 
         alert(nome + " ," + "você escolheu o número:" + " " + Numero_Escolhido01);
     }
 
-    else if ((Numero_Escolhido02 > Numero_Escolhido01) && (Numero_Escolhido02 > Numero_Escolhido03)) {
+    else if ((Numero_Escolhido02 >= Numero_Escolhido01) && (Numero_Escolhido02 >= Numero_Escolhido03)) {
 
         alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido02);
     }
 
 
-    else if ((Numero_Escolhido03 > Numero_Escolhido01) && (Numero_Escolhido03 > Numero_Escolhido02)) {
+    else if ((Numero_Escolhido03 >= Numero_Escolhido01) && (Numero_Escolhido03 >= Numero_Escolhido02)) {
 
         alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido03);
 
--- exercicio05.js
+++ exercicio05.js
@@ -4,37 +4,47 @@ let Numero_Escolhido01 = parseInt(prompt("Informe um número:"));
 let Numero_Escolhido02 = parseInt(prompt("Informe um número:"));
 let Numero_Escolhido03 = parseInt(prompt("Informe um número:"));
 //let mensagem = "";
-var maior =0;
-var menor ;
+let maior, menor;
 
 {
 
-    if ((Numero_Escolhido01 > Numero_Escolhido02) && (Numero_Escolhido01 > Numero_Escolhido03) || (Numero_Escolhido01r >= menor));
-        
-        
-        {
+    if ((Numero_Escolhido01 >= Numero_Escolhido02) && (Numero_Escolhido01 >= Numero_Escolhido03)) {
 
-        alert(nome + " ," + "você escolheu o número:" + " " + Numero_Escolhido01 + " E o número menor:" + menor);
+        maior = Numero_Escolhido01;
+    }
+
+    else if ((Numero_Escolhido02 >= Numero_Escolhido01) && (Numero_Escolhido02 >= Numero_Escolhido03)) {
+
+        maior = Numero_Escolhido02
     }
 
 
+    else if ((Numero_Escolhido03 >= Numero_Escolhido01) && (Numero_Escolhido03 >= Numero_Escolhido02)) {
+
+        maior = Numero_Escolhido03
+
+    }
 
 
+    if ((Numero_Escolhido01 <= Numero_Escolhido02) && (Numero_Escolhido01 <= Numero_Escolhido03)) {
+
+        menor = Numero_Escolhido01
+    }
+
+    else if ((Numero_Escolhido02 <= Numero_Escolhido01) && (Numero_Escolhido02 <= Numero_Escolhido03)) {
+
+        menor = Numero_Escolhido02
+    }
 
 
+    else if ((Numero_Escolhido03 <= Numero_Escolhido01) && (Numero_Escolhido03 <= Numero_Escolhido02)) {
 
-    //else if ((Numero_Escolhido02 > Numero_Escolhido01) && (Numero_Escolhido02 > Numero_Escolhido03)) {
+        menor = Numero_Escolhido03
 
-      //  alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido02);
-   // }
+        alert(nome + ", " + "você escolheu o maior número:" + " " + maior
+            + "E o menor número:" + " " + menor
+         );
+    }
 
-
-    //else if ((Numero_Escolhido03 > Numero_Escolhido01) && (Numero_Escolhido03 > Numero_Escolhido02)) {
-
-       // alert(nome + ", " + "você escolheu o número:" + " " + Numero_Escolhido03);
-
-   // }
 
 }
-
-
