9b08542fa7ec39f1994dcb50edee32ba7c74a8e0
joao3313 <ikkijp2012@gmail.com>
2024-08-26 19:30:51 -0300

    feat:Exercicio06

--- Exercicio06.js
+++ Exercicio06.js
@@ -1,15 +1,21 @@
+// feito pelo professor
+
 let nome = prompt("Informe seu nome:");
 
 let Ganhos_por_hora = parseFloat(prompt("Quanto você ganha por hora:"));
-let Horas_trabalhadas_mês = parseInt(prompt("Horas trabalhadas no mês:"));
-let Renda_mensal;
+let Horas_trabalhadas_mês = parseFloat(prompt("Horas trabalhadas no mês:"));
+let Renda_mensal = Ganhos_por_hora * Horas_trabalhadas_mês;
 
 
 
-if (Ganhos_por_hora * Horas_trabalhadas_mês);
+//if (Ganhos_por_hora * Horas_trabalhadas_mês);
+
+let mensagem = ' $ {}, recebeuR$ ${Valor total} no mês';
+
+alert (mensagem)
 
 
 
 
 
-alert(nome + ", " + "você escolheu o número:" + " " + Renda_mensal);
+//alert(nome + ", " + "você escolheu o número:" + " " + Renda_mensal);
