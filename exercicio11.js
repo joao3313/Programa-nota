0bba3e2e13f646c926cf63aedf61133d705598e1
joao3313 <ikkijp2012@gmail.com>
2024-08-28 19:51:25 -0300

    feat:Exercicio11

---
+++ exercicio11.js
@@ -0,0 +1,39 @@
+
+let conversao = ` Converter:
+                   1 - Fahrenheit
+                   2 - Celsius
+                   `;
+
+let numeroescolhido = parseInt(prompt(conversao));
+let Numero1 = parseInt(prompt("Informe um número:"));
+
+//let Numero1 = parseInt(prompt(conversao));
+//let Temperatura = parseFloat(prompt("Temperatura em graus :"));
+
+// let opcao1 = parseInt(prompt("Digite número para converter:"));
+
+var fahrenheit = 5 * (fahrenheit - 32) / 9;
+var celsius = 9 * (celsius / 5) + 32;
+
+
+
+
+
+switch (numeroescolhido) {
+
+
+
+    case 1:
+
+        numeroescolhido = (numeroescolhido) + (Numero1)
+        alert("Número convertido:  " = fahrenheit);
+        break;
+
+
+
+    case 2:
+        numeroescolhido = (numeroescolhido) - (Numero1)
+        alert("Número convertido:" + celsius);
+        break;
+
+}
